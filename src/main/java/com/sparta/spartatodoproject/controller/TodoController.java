6b6c2f245ab681ef5b5c64894013edec078bf424
dobam <ckehqja@gmail.com>
2024-05-26 11:52:48 +0900

    7단계 Swagger 활용, 유효성 검사

--- src/main/java/com/sparta/spartatodoproject/controller/TodoController.java
+++ src/main/java/com/sparta/spartatodoproject/controller/TodoController.java
@@ -17,15 +17,21 @@ import com.sparta.spartatodoproject.dto.TodoListResponseDto;
 import com.sparta.spartatodoproject.dto.TodoResponseDto;
 import com.sparta.spartatodoproject.service.TodoService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name="todo controller", description = "일정 관리 api")
 @RequiredArgsConstructor
 @RequestMapping("/todo")
 @RestController
 public class TodoController {
 	private final TodoService todoService;
 
+	@Operation(description = "일정 등록")
+	@Parameter(description = "dto를 받아 유효검사")
 	@PostMapping
 	public ResponseEntity<CommonResponse<TodoResponseDto>> addTodo(
 		@Valid @RequestBody TodoAddRequestDto requestDto) {
@@ -36,6 +42,8 @@ public class TodoController {
 			.data(responseDto).build());
 	}
 
+	@Operation(description = "일정 조회")
+	@Parameter(description = "경로값을 받아와서 조회")
 	@GetMapping("/{id}")
 	public ResponseEntity<CommonResponse<TodoResponseDto>> getTodoById(
 		@PathVariable("id") long id) {
@@ -46,6 +54,7 @@ public class TodoController {
 			.data(responseDto).build());
 	}
 
+	@Operation(description = "일정 전체 생성일 기준 내림차순으로 조회")
 	@GetMapping
 	public ResponseEntity<CommonResponse<TodoListResponseDto>> getAllTodos() {
 		return ResponseEntity.ok().body(CommonResponse.<TodoListResponseDto>builder()
@@ -54,9 +63,11 @@ public class TodoController {
 			.data(todoService.getTodoList()).build());
 	}
 
+	@Operation(description = "일정 수정, 비밀번호가 일치해야 함")
+	@Parameter(description = "수정할 일정을 경로로 받고 수정 dto 와 검증을 위한 비밀번호를 받아온다.")
 	@PutMapping("/{id}")
 	public ResponseEntity<CommonResponse<TodoResponseDto>> updateTodo(
-		@RequestBody TodoAddRequestDto requestDto, @PathVariable("id") long id) {
+		@Valid @RequestBody TodoAddRequestDto requestDto, @PathVariable("id") long id) {
 		TodoResponseDto responseDto = todoService.updateTodo(requestDto, id);
 		return ResponseEntity.ok().body(CommonResponse.<TodoResponseDto>builder()
 			.statusCode(HttpStatus.OK.value())
@@ -64,6 +75,8 @@ public class TodoController {
 			.data(responseDto).build());
 	}
 
+	@Operation(description = "일정 삭제, 비밀번호가 일치해야 함")
+	@Parameter(description = "삭제할 일정을 경로로 받고 검증을 위한 비밀번호를 받아온다.")
 	@DeleteMapping("/{id}")
 	public ResponseEntity<CommonResponse> deleteTodo(
 		String password, @PathVariable Long id) {
--- src/main/java/com/sparta/spartatodoproject/dto/TodoAddRequestDto.java
+++ src/main/java/com/sparta/spartatodoproject/dto/TodoAddRequestDto.java
@@ -1,13 +1,18 @@
 package com.sparta.spartatodoproject.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class TodoAddRequestDto {
 	@NotBlank
+	@Size(min = 1, max = 200)
 	private String title;
+	@Email
 	private String manager;
+	@NotBlank
 	private String password;
 	private String contents;
 
