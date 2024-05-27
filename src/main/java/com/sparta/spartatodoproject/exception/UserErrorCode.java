7af69a45962c8bdc364235f23e23a22375ce90bd
dobam <ckehqja@gmail.com>
2024-05-27 12:28:10 +0900

    user error code 추가

--- src/main/java/com/sparta/spartatodoproject/exception/UserErrorCode.java
+++ src/main/java/com/sparta/spartatodoproject/exception/UserErrorCode.java
@@ -9,7 +9,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum UserErrorCode implements ErrorCode {
 	USER_NOT_FOUND(HttpStatus.NOT_FOUND,
-		"유저가 없습니다.");
+		"유저가 없습니다."),
+	PW_MISMATCH(HttpStatus.NOT_FOUND,
+		"유저이름과 비밀번호 불일치");
 
 	private final HttpStatus httpStatus;
 	private final String message;
