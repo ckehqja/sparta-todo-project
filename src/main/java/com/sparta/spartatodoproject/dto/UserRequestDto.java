1d890de8f4dd7ce24d311fa122832f82d5e4cebd
dobam <ckehqja@gmail.com>
2024-05-31 17:44:11 +0900

    2-6, 회원가입 조건 추가

--- src/main/java/com/sparta/spartatodoproject/dto/UserRequestDto.java
+++ src/main/java/com/sparta/spartatodoproject/dto/UserRequestDto.java
@@ -1,13 +1,14 @@
 package com.sparta.spartatodoproject.dto;
 
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class UserRequestDto {
 	private String nickname;
-	@Size(min = 4, max = 10)
+	@Pattern(regexp = "^[A-Za-z0-9]{4,10}$", message = "대소문자, 숫자를 4~10자 사이 입력")
 	private String username;
+	@Pattern(regexp = "^[A-Za-z0-9]{8,15}$", message = "대소문자, 숫자를 8~15자 사이 입력")
 	private String password;
 	private String role;
 
